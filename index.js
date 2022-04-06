bc9e29cc1c8cf02c3b15eb8edd61e0cc9f472906
Marcos Martinez <mmtz2108@gmail.com>
2022-04-06 16:57:20 -0500

    commit tree b7c7718192f1d282e524372412bde82629b7c2e7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+console.log(cats);
+function destructivelyAppendCat(string) {
+    cats.push('Ralph');
+};
+function destructivelyPrependCat(string) {
+    cats.unshift('Bob');
+};
+function destructivelyRemoveLastCat(string) {
+    cats.pop('Ralph');
+};
+function destructivelyRemoveFirstCat(string) {
+    cats.shift('Bob');
+};
+function appendCat(string) {
+    return [...cats, 'Broom'];
+}
+function prependCat(string) {
+    return ['Arnold', ...cats];
+};
+function removeLastCat() {
+    return cats.slice(0, -1);
+};
+function removeFirstCat() {
+    return cats.slice(1);
+};
